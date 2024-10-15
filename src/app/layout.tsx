68d1f019ee5becd916056f3ec45a2b2af74a1627
Sileniz <walissonmacedo02@gmail.com>
2024-10-15 16:21:50 -0300

    add depedence analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Header/>
         {children}
         <Footer/>
+        <Analytics />
       </body>
     </html>
   );
